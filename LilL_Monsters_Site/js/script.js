91c8afd0658e77ca71bb54d41ac65524da4e664d
yangli <morganliyang@hotmail.com>
2024-12-28 20:04:45 -0500

    Revert "update to 2300"
    This reverts commit 6e86339b0e95a6dbad78d67e38c37e55f3fc950d.

--- LilL_Monsters_Site/js/script.js
+++ LilL_Monsters_Site/js/script.js
@@ -105,7 +105,7 @@ $(function () {
   new ScrollMagic.Scene({
     triggerElement: '#types',
     triggerHook: 0,
-    duration: 2300,
+    duration: 300,
   })
     .setPin('#types')
     .setTween(typesTween)
